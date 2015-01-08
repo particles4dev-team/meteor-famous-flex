2736a9496aedc24e9601146f3e4fd6badd1682ef
Jens Zastrow <jens.zastrow@mj-networks-gmbh.de>
2015-01-08 19:40:41 +0100

    upgrade to famous-flex 0.1.5. Don't need for using fork since 'npm run-script global-no-famous' is included in core.

---
+++ build-lib.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+version="0.1.5"
+tempDir=".build"
+
+rm -rf ${tempDir}
+mkdir -p ${tempDir}
+cd ${tempDir}
+wget https://github.com/IjzerenHein/famous-flex/archive/v${version}.tar.gz
+tar xfvz v${version}.tar.gz
+cd famous-flex-${version}
+npm install
+npm run-script global-no-famous
+cp global-no-famous.js ../../lib/flex-scrollview.js
+cd -
+cd -
