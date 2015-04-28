ee04d887307bc76572aab847a3ad2df51e0a4b3a
particle4dev <particle4dev@gmail.com>
2015-04-28 11:21:19 +0700

    upgrade famous flex 0.3.0

--- build-lib.sh
+++ build-lib.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version="0.2.0"
+version="0.3.0"
 tempDir=".build"
 
 rm -rf ${tempDir}
@@ -11,6 +11,7 @@ tar xfvz v${version}.tar.gz
 cd famous-flex-${version}
 npm install
 npm run-script global-no-famous
-cp global-no-famous.js ../../lib/flex-scrollview.js
+# https://github.com/IjzerenHein/famous-flex/commit/ef168a8753668b41204d3ab3d3677a9b041e496f
+cp ./dist/famous-flex-global.js ../../lib/flex-scrollview.js
 cd -
 cd -
